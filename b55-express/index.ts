1f7b8bf860d6fb30d91954a1f36d2350efafae03
maulanahari <harimaulana.xyz@gmail.com>
2024-09-02 20:57:25 +0700

    new

--- b55-express/index.ts
+++ b55-express/index.ts
@@ -246,11 +246,6 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT;
 
-import { VercelRequest, VercelResponse } from '@vercel/node';
-export default (req: VercelRequest, res: VercelResponse) => {
-  app(req as any, res as any);
-};
-
-// app.listen(port, () => {
-//   console.log(`Server berjalan di port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server berjalan di port ${port}`);
+});
