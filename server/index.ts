ec5dbaff6ecc03cbd27d612912762d2f37fc44ac
maulanahari <harimaulana.xyz@gmail.com>
2024-11-04 20:52:02 +0700

    change cors for local dev

--- server/index.ts
+++ server/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://cicrcleapp-hm.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
