8efe8db30ba1eca1757e14b6b597dde0ac598bba
Hari Maulana <harimaulana.xyz@gmail.com>
2024-11-11 20:21:20 +0700

    edit branding logo

--- client/src/components/commons/LogoBrand.tsx
+++ client/src/components/commons/LogoBrand.tsx
@@ -1,14 +1,18 @@
-import { Typography } from '@mui/material'
-import { theme } from '../../Themes'
+import { Typography } from "@mui/material";
+import { theme } from "../../Themes";
 
 const LogoBrand = () => {
   return (
     <>
-        <Typography fontSize={"40px"} fontFamily={"arial"} sx={{margin: "0", padding: "0", color: theme.palette.text.secondary}}>
-        Cir<span style={{color: theme.palette.primary.main}}>c</span>le
-        </Typography>
+      <Typography
+        fontSize={"40px"}
+        fontFamily={"arial"}
+        sx={{ margin: "0", padding: "0", color: theme.palette.text.secondary }}
+      >
+        ascend.
+      </Typography>
     </>
-  )
-}
+  );
+};
 
-export default LogoBrand
+export default LogoBrand;
